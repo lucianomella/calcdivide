552ad4a0477657ca46d04eedb556ff35824aa53f
Luciano Mella <lucianomella@gmail.com>
2020-11-24 23:41:11 -0300

    Se agrega endpoint coverage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ let app = express();
 
 app.use(express.json());
 
+app.use( '/coverage',express.static('./coverage/lcov-report/'));
+
 app.use('/divide', require('./routes/divide'));
 
 app.listen(PORT);
