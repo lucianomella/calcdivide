5f65a4bf052684e657e1e6d69ccc4beecdc89f41
Luciano Mella <lucianomella@gmail.com>
2020-11-24 23:37:59 -0300

    Se agrega funcionalidad para endpoint divide

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+
+const PORT = process.env.PORT || 3001;
+let app = express();
+
+app.use(express.json());
+
+app.use('/divide', require('./routes/divide'));
+
+app.listen(PORT);
+console.log(`Express server listening on port ${PORT}`);
+
+module.exports = app
