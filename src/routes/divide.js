f84bbdb96ed6131217568758c6a6556af579fbc9
Luciano Mella <lucianomella@gmail.com>
2020-11-24 23:39:49 -0300

    Correccion en funcion de endpoint

--- src/routes/divide.js
+++ src/routes/divide.js
@@ -5,7 +5,7 @@ const router = Router();
 router.post("/", (req, res) => {
     let body = req.body;
     if (utils.validaDatos(body)) {
-        return res.status(200).send(String(Number(body.a) + Number(body.b)));
+        return res.status(200).send(String(Number(body.a) / Number(body.b)));
     } else {
         return res.status(400).send("Error en datos");
     }
